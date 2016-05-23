2236e85d1eb2cc8d9c65319ae1754a300ae6d557
¨Jordan <jpoles1@gmail.com>
2016-05-23 10:02:17 -0400

    Fixed up a lot of issues: Changed links so images are referenced from the proper folder (images not css/images) Moved around scripts in HTML file, and added jquery on-load to javscript file. Fixed up some small css stuff.

--- hpd.js
+++ hpd.js
@@ -1,26 +1,28 @@
-var acc = document.getElementsByClassName("accordion");
-var i;
+$(function(){
+  var acc = document.getElementsByClassName("accordion");
+  var i;
 
-for (i = 0; i < acc.length; i++) {
-    acc[i].onclick = function(){
-    	  
-        this.classList.toggle("active");
-        this.nextElementSibling.classList.toggle("show");
-    }
-}
-/*test = new Date()
-month = test.getMonth()
-month = (month * 1) + 1
-day = test.getDate()
-year = test.getFullYear()
-document.write(" ",month,"/",day,"/",year," ")*/
+  for (i = 0; i < acc.length; i++) {
+      acc[i].onclick = function(){
 
-var elementPosition = $('#navbar-title').offset();
+          this.classList.toggle("active");
+          this.nextElementSibling.classList.toggle("show");
+      }
+  }
+  /*test = new Date()
+  month = test.getMonth()
+  month = (month * 1) + 1
+  day = test.getDate()
+  year = test.getFullYear()
+  document.write(" ",month,"/",day,"/",year," ")*/
 
-$(window).scroll(function(){
-        if($(window).scrollTop() > elementPosition.top){
-            $('#navbar-title').addClass('pastBanner')
-        } else {
-            $('#navbar-title').removeClass('pastBanner');
-        }    
-});
+  var elementPosition = $('#navbar-title').offset();
+
+  $(window).scroll(function(){
+          if($(window).scrollTop() > elementPosition.top){
+              $('#navbar-title').addClass('pastBanner')
+          } else {
+              $('#navbar-title').removeClass('pastBanner');
+          }
+  });
+})
